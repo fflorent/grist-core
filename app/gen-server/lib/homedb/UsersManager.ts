1d90cea869f07f73f27eab8eec1956f08c5533fe
fflorent <florent.git@zeteo.me>
2024-09-07 11:59:12 +0200

    Add tests for the new UsersManager methods

--- app/gen-server/lib/homedb/UsersManager.ts
+++ app/gen-server/lib/homedb/UsersManager.ts
@@ -582,6 +582,11 @@ export class UsersManager {
     });
   }
 
+  public async getUsers() {
+    return await User.find({relations: ["logins"]});
+  }
+
+
   /**
    * ==================================
    *
@@ -676,10 +681,6 @@ export class UsersManager {
    */
   public getExcludedUserIds(): number[] {
     return [this.getSupportUserId(), this.getAnonymousUserId(), this.getEveryoneUserId()];
-  }
-
-  public async getUsers() {
-    return await User.find({relations: ["logins"]});
   }
 
   /**
