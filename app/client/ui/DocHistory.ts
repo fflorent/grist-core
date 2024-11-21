6abe4121c3ebb2c4bc8a9e0367255ba7e858751c
fflorent <florent.git@zeteo.me>
2024-11-21 09:26:33 +0100

    WIP

--- app/client/ui/DocHistory.ts
+++ app/client/ui/DocHistory.ts
@@ -1,4 +1,4 @@
-import {makeT} from 'app/client/lib/localization';
+import {detectCurrentLang, makeT} from 'app/client/lib/localization';
 import {createSessionObs} from 'app/client/lib/sessionObs';
 import {getTimeFromNow} from 'app/client/lib/timeUtils';
 import {DocPageModel} from 'app/client/models/DocPageModel';
@@ -13,7 +13,6 @@ import {buildUrlId, parseUrlId} from 'app/common/gristUrls';
 import {StringUnion} from 'app/common/StringUnion';
 import {DocSnapshot} from 'app/common/UserAPI';
 import {Disposable, dom, IDomComponent, MultiHolder, Observable, styled} from 'grainjs';
-import moment from 'moment';
 
 const t = makeT('DocHistory');
 
@@ -21,9 +20,25 @@ const DocHistorySubTab = StringUnion("activity", "snapshots");
 
 export class DocHistory extends Disposable implements IDomComponent {
   private _subTab = createSessionObs(this, "docHistorySubTab", "snapshots", DocHistorySubTab.guard);
+  private _dateFormatter;
+  private _relativeTimeFormatter;
 
   constructor(private _docPageModel: DocPageModel, private _actionLog: IDomComponent) {
     super();
+    const currentLang = detectCurrentLang();
+    this._dateFormatter = new Intl.DateTimeFormat(currentLang, {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+    this._relativeTimeFormatter = new Intl.RelativeTimeFormat(currentLang, {
+      numeric: "always", // Don't use "auto", it is confusing when
+    });
+    // FIXME: remove this
+    (window as any).testMe = this._formatRelTime.bind(this);
   }
 
   public buildDom() {
@@ -95,18 +110,19 @@ export class DocHistory extends Disposable implements IDomComponent {
         testId('doc-history-error'))),
       // Note that most recent snapshots are first.
       dom.domComputed(snapshots, (snapshotList) => snapshotList.map((snapshot, index) => {
-        const modified = moment(snapshot.lastModified);
+        const formattedLastModified = this._dateFormatter.format(new Date(snapshot.lastModified));
         const prevSnapshot = snapshotList[index + 1] || null;
+        const isSelected = Boolean(
+          snapshot.snapshotId === doc.idParts.snapshotId ||
+            (compareSnapshotId && snapshot.snapshotId === compareSnapshotId)
+        );
+        const isCurrent = index === 0;
         return cssSnapshot(
           cssSnapshotTime(getTimeFromNow(snapshot.lastModified)),
           cssSnapshotCard(
-            cssSnapshotCard.cls('-current', Boolean(
-              snapshot.snapshotId === doc.idParts.snapshotId ||
-              (compareSnapshotId && snapshot.snapshotId === compareSnapshotId)
-            )),
-            dom('div',
-              cssDatePart(modified.format('ddd ll')), ' ',
-              cssDatePart(modified.format('LT'))
+            cssSnapshotCard.cls('-active', isSelected),
+            dom('div', {title: formattedLastModified}, isCurrent ? t('Current version') :
+              cssDatePart(formattedLastModified)
             ),
             cssMenuDots(icon('Dots'),
               menu(() => [
@@ -126,6 +142,28 @@ export class DocHistory extends Disposable implements IDomComponent {
         );
       })),
     );
+  }
+
+  private _formatRelTime(seconds: number) {
+    const upgradeUnitsWhenValueAbove: { [unitName: string]: number } = {
+      seconds: 60,
+      minutes: 60,
+      hours: 24,
+      days: 30,
+      months: 12,
+      year: Infinity
+    };
+
+    let curValue = seconds;
+    for (const [unitName, valueForUpgrade] of Object.entries(upgradeUnitsWhenValueAbove)) {
+      // FIXME: maybe add or substract value for some units so we can use numeric: "auto".
+      // Say we are the 15th of October, we should display "last month" when the value is for between 1st and 15th of September.
+      if (Math.abs(curValue) < valueForUpgrade) {
+        return this._relativeTimeFormatter.format(curValue, unitName as Intl.RelativeTimeFormatUnit);
+      }
+      curValue = Math.round(curValue / valueForUpgrade);
+    }
+    return this._relativeTimeFormatter.format(curValue, 'year');
   }
 }
 
@@ -161,7 +199,7 @@ const cssSnapshotCard = styled('div', `
   align-items: center;
   --icon-color: ${theme.controlSecondaryFg};
 
-  &-current {
+  &-active {
     background-color: ${theme.documentHistorySnapshotSelectedBg};
     color: ${theme.documentHistorySnapshotSelectedFg};
     --icon-color: ${theme.documentHistorySnapshotSelectedFg};
